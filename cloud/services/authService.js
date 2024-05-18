3f7aa74f00ae461fc700fbc43621dc00c6098bf5
Михаел Йорданов <gaws@mail.bg>
2024-05-18 11:52:46 +0300

    implement authService with signIn and signUp functionallity

--- cloud/services/authService.js
+++ cloud/services/authService.js
@@ -1,8 +1,19 @@
 const supabase = require("../configs/supabaseConfig");
 
 function signIn(email, password) {
-  return supabase.auth.signIn({
+  return supabase.auth.signInWithPassword({
     email,
     password,
   });
 }
+
+function signUp(email, password) {
+  return supabase.auth.signUp({
+    email,
+    password,
+  });
+}
+module.exports = {
+  signIn,
+  signUp,
+};
