99624b076ec83354b4e7495b64ccf935b5dce56e
Михаел Йорданов <gaws@mail.bg>
2024-05-18 11:55:03 +0300

    Add route for auth

--- cloud/utils/validationSchemas.js
+++ cloud/utils/validationSchemas.js
@@ -1,3 +1,13 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 // Write schemas for validation of data, for more info - https://joi.dev/api/?v=17.13.0
+
+export const loginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
+
+export const registerSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+});
