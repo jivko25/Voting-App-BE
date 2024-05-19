f61e97819f62de9e05e4405445f595e2f23ec1f0
Михаел Йорданов <gaws@mail.bg>
2024-05-19 23:26:19 +0300

    add auth controller with login logic

--- cloud/controllers/authController.js
+++ cloud/controllers/authController.js
@@ -1,28 +1,32 @@
 const router = require("express").Router();
 const authServices = require("../services/authService");
-// const joi = require("joi");
-
-// const loginSchema = joi.object({
-//   email: joi.string().email().required(),
-//   password: joi.string().required(),
-// });
+const { loginSchema, registerSchema } = require("../utils/validationSchemas");
+const e = require("express");
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  //   const error = loginSchema.validate({ email, password });
-  //   if (error) {
-  //     const errorMessage = error.error?.details[0].message;
-  //     return res.status(400).json({ message: errorMessage });
-  //   }
+  const { error } = loginSchema.validate({ email, password });
+
+  if (error) {
+    const errorMessage = error?.details[0].message;
+    return res.status(400).json({ message: errorMessage });
+  }
   try {
-    const data = await authServices.signIn(email, password);
-    if (!data) {
+    const { data, error } = await authServices.signIn(email, password);
+    console.log(error);
+    if (error) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
-    if (data?.error) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-    return res.status(200).json(data);
+    const { user, session } = data;
+    const signInResponse = {
+      id: user?.id,
+      email: user?.email,
+      role: user?.role,
+      access_token: session?.access_token,
+      refresh_token: session?.refresh_token,
+      expires_in: session?.expires_in,
+    };
+    return res.status(200).json(signInResponse);
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "An unexpected error occurred" });
