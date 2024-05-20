adf28b67d7bb1e163feefd6f501d7b3ec94cd576
Михаел Йорданов <gaws@mail.bg>
2024-05-20 16:02:47 +0300

    add first and last name to validation schema

--- cloud/utils/validationSchemas.js
+++ cloud/utils/validationSchemas.js
@@ -10,6 +10,8 @@ const loginSchema = Joi.object({
 const registerSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(8).required(),
+  firstName: Joi.string().min(2).required(),
+  lastName: Joi.string().min(2).required(),
 });
 
 module.exports = {
