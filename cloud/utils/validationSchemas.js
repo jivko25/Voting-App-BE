f3b096f71f9cf1836ca8831600bcf267e8aa9a3b
Михаел Йорданов <gaws@mail.bg>
2024-05-21 10:37:35 +0300

    Add password regex to register validation schema

--- cloud/utils/validationSchemas.js
+++ cloud/utils/validationSchemas.js
@@ -2,6 +2,9 @@ const Joi = require("joi");
 
 // Write schemas for validation of data, for more info - https://joi.dev/api/?v=17.13.0
 
+//Ensures that the password is at least 8 characters long and contains at least one uppercase letter, one lowercase letter, one number, and one special character
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/;
+
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
@@ -9,7 +12,11 @@ const loginSchema = Joi.object({
 
 const registerSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
+  password: Joi.string().pattern(passwordRegex).required().messages({
+    "string.pattern.base":
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character and be at least 8 characters long",
+  }),
+
   firstName: Joi.string().min(2).required(),
   lastName: Joi.string().min(2).required(),
 });
