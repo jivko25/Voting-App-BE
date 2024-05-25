dfe078c5542e43f232ada54310b416a727c194db
ajummer <makeudreamempty@gmail.com>
2024-05-25 22:05:41 +0300

    add pkce flowtype in supabaseconfig

--- cloud/configs/supabaseConfig.js
+++ cloud/configs/supabaseConfig.js
@@ -1,8 +1,15 @@
 const { createClient } = require("@supabase/supabase-js");
 
+
 // Initialize Supabase
 const supabaseUrl = "https://ljudvicwjqokilajymhm.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqdWR2aWN3anFva2lsYWp5bWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2NzM2ODYsImV4cCI6MjAzMTI0OTY4Nn0.S_g4ETh7Pq5llUuRnH-GV8GkWiiC3oH4hisCWx_NxUg";
-const supabaseConfig = createClient(supabaseUrl, supabaseKey);
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqdWR2aWN3anFva2lsYWp5bWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2NzM2ODYsImV4cCI6MjAzMTI0OTY4Nn0.S_g4ETh7Pq5llUuRnH-GV8GkWiiC3oH4hisCWx_NxUg";
+const supabaseConfig = createClient(supabaseUrl, supabaseKey, {
+  // change flowtype to pkce , because we need  auth code in callback url in reset password to create a session 
+  auth: {
+    flowType: "pkce",
+  },
+});
 
 module.exports = supabaseConfig;
