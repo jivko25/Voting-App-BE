ba4b679ff2774c63d53c12a98e415677461f9c63
ajummer <makeudreamempty@gmail.com>
2024-05-25 22:08:41 +0300

    Add validation schemas for forgotPassword & resetPassword

--- cloud/utils/validationSchemas.js
+++ cloud/utils/validationSchemas.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 
+
 // Write schemas for validation of data, for more info - https://joi.dev/api/?v=17.13.0
 
 //Ensures that the password is at least 8 characters long and contains at least one uppercase letter, one lowercase letter, one number, and one special character
@@ -21,7 +22,20 @@ const registerSchema = Joi.object({
   lastName: Joi.string().min(2).required(),
 });
 
+const forgotPasswordSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+const resetPasswordSchema = Joi.object({
+  newPassword: Joi.string().pattern(passwordRegex).required().messages({
+    "string.pattern.base":
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character and be at least 8 characters long",
+  })
+})
+
 module.exports = {
   loginSchema,
   registerSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema
 };
